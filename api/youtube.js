6aefaf6d07aa4695635afeea37ce53ca33df15d3
Shivam Singh <ss8217170@gmail.com>
2026-08-16 06:50:22 +0530

    debug youtube api response

--- api/youtube.js
+++ api/youtube.js
@@ -1,33 +1,32 @@
 export default async function handler(req, res) {
 
-    const playlistId =
-        req.query.playlist;
+    const playlistId = req.query.playlist;
 
-
-    const key =
-        process.env.YOUTUBE_API_KEY;
-
+    const key = process.env.YOUTUBE_API_KEY;
 
     const url =
         `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=${playlistId}&key=${key}`;
 
 
-    const response =
-        await fetch(url);
+    const response = await fetch(url);
+
+    const data = await response.json();
 
 
-    const data =
-        await response.json();
+    console.log(data);
 
 
-    const songs =
-        data.items.map(item => ({
-            id:
-                item.snippet.resourceId.videoId,
+    if (!data.items) {
+        return res.status(500).json({
+            error: data
+        });
+    }
 
-            title:
-                item.snippet.title
-        }));
+
+    const songs = data.items.map(item => ({
+        id: item.snippet.resourceId.videoId,
+        title: item.snippet.title
+    }));
 
 
     res.status(200).json(songs);
